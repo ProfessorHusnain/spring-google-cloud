328b592d5b5e62c868ba047300a4c072e061e40b
ProfessorHusnain <mh571536@gmail.com>
2023-04-27 11:58:49 +0500

    spring boot for google cloud accesing

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+@RestController
+@RequestMapping
+class controller{
+    @GetMapping
+    public String hello(){
+        return "google Cloud";
+    }
+    @GetMapping("/v1")
+    public String hello1(){
+        return "google Cloud with v1";
+    }
+
+}
